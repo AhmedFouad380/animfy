ae7c99a1e70694abdc0d0f46e2f0938d5bb7d522
AhmedFouad380 <36769581+AhmedFouad380@users.noreply.github.com>
2026-05-18 15:31:45 +0300

    a

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -12,13 +13,13 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
-    }
+     }
 
     /**
      * Bootstrap any application services.
      */
     public function boot(): void
     {
-        //
+        Schema::defaultStringLength(191);
     }
 }
